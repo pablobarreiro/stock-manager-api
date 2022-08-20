b0c0646c192c269b0052fdcb1b64431e7317ebff
pablobarreiro <pbarreiro92@gmail.com>
2022-08-20 00:25:23 -0300

    ssl test2

--- db/index.js
+++ db/index.js
@@ -1,8 +1,10 @@
-const Sequelize = require('sequelize');
-require('dotenv').config({path:'.env'});
+const Sequelize = require("sequelize");
+require("dotenv").config({ path: ".env" });
 
-const db = new Sequelize(process.env.DATABASE_URL,{
-  dialectOptions: {}
-})
+const db = new Sequelize(process.env.DATABASE_URL, {
+  logging: false,
+  ssl: true,
+  dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
+});
 
-module.exports = db
+module.exports = db;
