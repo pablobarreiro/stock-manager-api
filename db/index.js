1b6b0ec57e8101f037bfbcf3fedcb3076aa14d17
pablobarreiro <pbarreiro92@gmail.com>
2022-08-20 00:21:53 -0300

    ssl test

--- db/index.js
+++ db/index.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize');
 require('dotenv').config({path:'.env'});
 
-const db = new Sequelize(process.env.DATABASE_URL)
+const db = new Sequelize(process.env.DATABASE_URL,{
+  dialectOptions: {}
+})
 
 module.exports = db
